46041ec53a18f26a6b215b21cba8767bb787bf27
Olga Tsybikova <tsy2701@gmail.com>
2022-05-15 16:56:40 +0200

    Installed and enabled cors in server index

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 const { robotRouter } = require("./routers/robotRouter");
 
 const app = express();
-
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/robots", robotRouter);
