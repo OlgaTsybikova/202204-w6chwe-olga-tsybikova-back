e1dd4be747b322881efe39768d0331406adea02d
Olga Tsybikova <tsy2701@gmail.com>
2022-05-15 14:01:52 +0200

    tested controller

--- db/model/Robot.js
+++ db/model/Robot.js
@@ -1,8 +1,21 @@
 const { Schema, model } = require("mongoose");
 
 const RobotSchema = new Schema({
-  name: { type: String },
-  image: { type: String },
+  name: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  speed: {
+    type: Number,
+  },
+  resilience: {
+    type: Number,
+  },
+  created: {
+    type: Number,
+  },
 });
 
 const Robot = model("Robot", RobotSchema, "robots");
