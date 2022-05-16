1e16e542a1197e696d901c83ca54361f8ed6a8d0
Olga Tsybikova <tsy2701@gmail.com>
2022-05-16 20:52:06 +0200

    Created auth without user, deleted user schema

---
+++ server/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+  try {
+    if (!authorization.includes("Bearer ")) {
+      throw new Error();
+    }
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRETO);
+    next();
+  } catch {
+    const customError = new Error("Invalid token");
+    customError.statusCode = 401;
+    next(customError);
+  }
+};
+module.exports = auth;
