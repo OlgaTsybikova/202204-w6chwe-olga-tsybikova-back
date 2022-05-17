7b764026a0b1ba7424b56fb597ec55eee3578e8e
Olga Tsybikova <tsy2701@gmail.com>
2022-05-17 18:27:20 +0200

    Created user schema

---
+++ db/model/User.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+});
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
